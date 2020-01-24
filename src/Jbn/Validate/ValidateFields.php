10854a593690c0ecc6b89e600c7503a1455a9872
John Bright <brightnyamata@gmail.com>
2020-01-24 14:36:02 +0000

    code refactored

--- src/Jbn/Validate/ValidateFields.php
+++ src/Jbn/Validate/ValidateFields.php
@@ -20,44 +20,44 @@ namespace Jbn\Validate;
 
 
         /**
-    * @param array $defaultFields array of fields that has to be checked.
-    * @param array $fieldsFromUser array of fields submitted by the user
+    * @param array $requiredFields array of fields that has to be checked. eg ["username"=>20, "email"=>60] Interger values are the fields' max length
+    * @param array $requestObject array of fields submitted by the user. eg  ["username"=>"foo", "password"=>"bar"]
     * @param array $customMessage OPTIONAL speficies custom message to return for each field when validation fails
     * @return array returns array of error true with error message if a field fails the validation or false and the values if all the fields pass the validation
     */
-     static function validate(array $defaultFields, array $fieldsFromUser, array $customMessage = null) : array
+     static function validate(array $requiredFields, array $requestObject, array $customMessage = null) : array
     {
-        if (!$fieldsFromUser == null) {
-            $fieldsFromUserKeys = array_keys($fieldsFromUser); //extract all the keys from the array submited by user
-            $defaultFieldsKeys = array_keys($defaultFields);
+        if (!$requestObject == null) {
+            $requestObjectKeys = array_keys($requestObject); //extract all the keys from the array submited by user
+            $requiredFieldsKeys = array_keys($requiredFields);
             $newarray = []; //create new array
-            foreach ($defaultFields as $fieldsKey => $fieldLength) { //loop through the fields to check
-                foreach ($fieldsFromUser as $firstnameKey => $values) { //loop through the fields submitted by the user
-                    if (!in_array($fieldsKey, $fieldsFromUserKeys)) { //if a default field set is not in the fields submitted by the user, throw an error
-                        return ["error" => true, "message" => " " . str_replace('_', " ", $fieldsKey) . " is required"];
-                    } elseif (in_array($fieldsKey, $fieldsFromUserKeys) && $fieldsKey == $firstnameKey) { 
+            foreach ($requiredFields as $requiredFieldsKey => $fieldLength) { //loop through the fields to check
+                foreach ($requestObject as $requestObjectKey => $values) { //loop through the fields submitted by the user
+                    if (!in_array($requiredFieldsKey, $requestObjectKeys)) { //if a default field set is not in the fields submitted by the user, throw an error
+                        return ["error" => true, "message" => " " . str_replace('_', " ", $requiredFieldsKey) . " is required"];
+                    } elseif (in_array($requiredFieldsKey, $requestObjectKeys) && $requiredFieldsKey == $requestObjectKey) { 
                         
                         //check the field length
                         if(strlen($values) > $fieldLength){
                             if($customMessage != null) //if custom message is not null, return custom message instead
-                             return ValidateFields::customMessage($customMessage, $fieldsKey, $defaultFieldsKeys);
+                             return ValidateFields::customMessage($customMessage, $requiredFieldsKey, $requiredFieldsKeys);
 
-                            return ["error" => true, "message" => "" . str_replace('_', " ", $fieldsKey) . " should be ".$fieldLength." characters or less"];
+                            return ["error" => true, "message" => "" . str_replace('_', " ", $requiredFieldsKey) . " should be ".$fieldLength." characters or less"];
                         }
                         
                         if (empty(trim($values))) {//if the field submitted by the user is empty, prompt the user
                             if($customMessage != null) //if custom message is not null, return custom message instead
-                             return ValidateFields::customMessage($customMessage, $fieldsKey, $defaultFieldsKeys);
+                             return ValidateFields::customMessage($customMessage, $requiredFieldsKey, $requiredFieldsKeys);
 
-                            return ["error" => true, "message" => "" . str_replace('_', " ", $fieldsKey) . " should not be empty"];
+                            return ["error" => true, "message" => "" . str_replace('_', " ", $requiredFieldsKey) . " should not be empty"];
                         }else{
-                            if($fieldsKey == "email"){ //validate email address
+                            if($requiredFieldsKey == "email"){ //validate email address
                                 if(!ValidateFields::filterEmail($values)){
                                     return ["error" => true, "message" => "Invalid email address"];
                                 }
                             }
                             //create new array with values and their keys
-                            $newarray = array_merge($newarray, ["error"=> false, $fieldsKey => $values]);//create new array obj with specified fields and corresponding vaues
+                            $newarray = array_merge($newarray, ["error"=> false, $requiredFieldsKey => $values]);//create new array obj with specified fields and corresponding vaues
                         }
                     }
                 }
@@ -68,9 +68,8 @@ namespace Jbn\Validate;
         }
     }
 
-    private static function customMessage($customMessage, $fieldsKey, $defaultFieldsKeys){
-
-            $index = array_search($fieldsKey, $defaultFieldsKeys); //get the index of the field
+    private static function customMessage($customMessage, $requiredFieldsKey, $requiredFieldsKeys){
+            $index = array_search($requiredFieldsKey, $requiredFieldsKeys); //get the index of the field
             return ["error" => true, "message" => $customMessage[$index]];
     }
 }
